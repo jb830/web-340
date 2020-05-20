41fe788df35fb294d9f94254fa35041aab928c92
Joanna <jbrumfield623@gmail.com>
2020-05-20 13:14:59 -0700

    Added exercise 4.2

---
+++ week-4/json-api/brumfield-4.2.js
@@ -0,0 +1,32 @@
+/*
+============================================
+; Title:  brumfield-3.3.js 
+; Author: Professor Krasso
+; Date: 13 May, 2020
+; Modified by: Joanna Brumfield
+; Description: Demonstrates creating 
+; a JSON API
+;===========================================
+*/
+const header = require('../../brumfield-header.js');
+console.log(header.display('Joanna', 'Brumfield', 'Exercise 3.4')+ '\n');
+
+const express = require('express');
+const http = require('http');
+//start app
+const app = express();
+//great GET reqest
+app.get('/customer/:id', function(req, res) {
+  let id = parseInt(req.params.id, 10)
+  //create JSON data to return to client
+  res.json({
+    firstName: 'Jo',
+    lastName: 'Brumfield',
+    employeeID: id
+  });
+});
+
+http.createServer(app).listen(3030, function() {
+  console.log('App has started on port 3030');
+});
+
